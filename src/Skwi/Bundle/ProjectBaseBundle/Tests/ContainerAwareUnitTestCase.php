7d9a469cfe6f6eda4d3fb1d55f42b6a874a62975
skwi <cedric.spalvieri@gmail.com>
2013-06-17 22:59:16 +0200

    Phpunit init

--- src/Skwi/Bundle/ProjectBaseBundle/Tests/ContainerAwareUnitTestCase.php
+++ src/Skwi/Bundle/ProjectBaseBundle/Tests/ContainerAwareUnitTestCase.php
@@ -2,27 +2,9 @@
 
 namespace Skwi\Bundle\ProjectBaseBundle\Test;
 
-require_once __DIR__.'/../../../../../app/AppKernel.php';
-
 abstract class ContainerAwareUnitTestCase extends \PHPUnit_Framework_TestCase
 {
-    protected static $kernel;
-    protected static $container;
-
     protected $isEntityNew;
-
-    public static function setUpBeforeClass()
-    {
-        self::$kernel = new \AppKernel('dev', true);
-        self::$kernel->boot();
-
-        self::$container = self::$kernel->getContainer();
-    }
-
-    public function get($serviceId)
-    {
-        return self::$kernel->getContainer()->get($serviceId);
-    }
 
     protected function getEmMock()
     {
