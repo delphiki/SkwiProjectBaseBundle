c22c4c06543742f66e6f97009682ea7a62bce950
jeremy <jeremy@novaway.fr>
2015-04-03 11:32:11 +0200

    Allow array parameter as query criteria

--- src/Skwi/Bundle/ProjectBaseBundle/Manager/BaseManager.php
+++ src/Skwi/Bundle/ProjectBaseBundle/Manager/BaseManager.php
@@ -486,8 +486,9 @@ abstract class BaseManager
         }
 
         foreach ($criteria as $key => $value) {
-            $qb->andWhere(sprintf('o.%1$s = :%1$s', $key))
-               ->setParameter($key, $value);
+            $operator = (is_array($value)) ? 'IN' : '=';
+            $qb->andWhere(sprintf('o.%1$s %2$s (:%1$s)', $key, $operator))
+                ->setParameter($key, $value);
         }
 
         $pager = $this->getPagerFromQueryBuilder($qb, $maxPerPage);
@@ -507,8 +508,9 @@ abstract class BaseManager
         $qb = $this->createBaseQueryBuilder($onlyActive);
 
         foreach ($criteria as $key => $value) {
-            $qb->andWhere(sprintf('o.%1$s = :%1$s', $key))
-               ->setParameter($key, $value);
+            $operator = (is_array($value)) ? 'IN' : '=';
+            $qb->andWhere(sprintf('o.%1$s %2$s (:%1$s)', $key, $operator))
+                ->setParameter($key, $value);
         }
 
         if (null !== $sortField) {
